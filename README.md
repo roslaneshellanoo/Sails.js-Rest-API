8b94730ffa2564fc1999a555e6b995c79576354b
roslanelyashiv <roslane@shellanoo.com>
2017-04-01 14:10:51 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quiz-rest
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,17 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side logic for managing customers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  'new': function (req, res) {
+
+    res.view();
+
+  },
+
+};
+
